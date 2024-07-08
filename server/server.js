c3bad7936025112d9f65fdaffd3ce01ee38c9001
shadowind797 <shadowind797@gmail.com>
2024-07-08 15:24:56 +0500

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -48,63 +48,101 @@ server.get(ITEMS_ENDPOINT, (req, res) => {
       const items = JSON.parse(content);
       if (text) {
         if (text.includes(" ")) {
-          const searched = items.filter((item) =>
-            item.name.toLowerCase().includes(text.split(" ")[0])
-          );
-          const moreSearched = searched.filter((item) =>
-            item.name.toLowerCase().includes(text.split(" ")[1])
-          );
-          const thirdSearched = items.filter((item) =>
-            item.name.toLowerCase().includes(text.split(" ")[1])
-          );
-          const searchLVL1 = text.split(" ")[1];
-          if (searched.length === 0 && !searchLVL1.includes(" ")) {
-            const afterSearch = thirdSearched;
-            res.status(200).json(afterSearch);
-          } else if (moreSearched.length === 0 && thirdSearched.length === 0) {
-            const searchLVL1 = text.split(" ")[1];
-            if (searchLVL1.includes(" ")) {
-              const searchedLVL1 = items.filter((item) =>
-                item.name.toLowerCase().includes(searchLVL1.split(" ")[0])
+          const secondWord = text.split(" ")[1];
+          if (secondWord.includes(" ")) {
+            const firstWord = text.split(" ")[0];
+            const secondWord = text.split(" ")[1];
+            const thirdWord = secondWord.split(" ")[1];
+
+            const firstSearch = items.filter((item) =>
+              item.name.toLowerCase().includes(firstWord)
+            );
+            if (firstSearch.length > 0) {
+              const secondSearch = firstSearch.filter((item) =>
+                item.name.toLowerCase().includes(secondWord)
               );
-              const moreSearchedLVL1 = searchedLVL1.filter((item) =>
-                item.name.toLowerCase().includes(searchLVL1.split(" ")[1])
-              );
-              const thirdSearchedLVL1 = items.filter((item) =>
-                item.name.toLowerCase().includes(searchLVL1.split(" ")[1])
-              );
-              if (searchedLVL1.length === 0 && thirdSearchedLVL1.length === 0) {
-                if (moreSearched.length === 0) {
-                  const afterSearch = searched;
-                  res.status(200).json(afterSearch);
+              if (secondSearch.length > 0) {
+                const thirdSearch = secondSearch.filter((item) =>
+                  item.name.toLowerCase().includes(thirdWord)
+                );
+                if (thirdSearch.length > 0) {
+                  res.status(200).json(thirdSearch);
                 } else {
-                  const afterSearch = moreSearched;
-                  res.status(200).json(afterSearch);
+                  const search9 = firstSearch.filter((item) =>
+                    item.name.toLowerCase().includes(thirdWord)
+                  );
+                  if (search9.length > 0) {
+                    res.status(200).json([...secondSearch, ...search9]);
+                  } else {
+                    res.status(200).json(secondSearch);
+                  }
                 }
-              } else if (moreSearchedLVL1.length === 0) {
-                const afterSearch = searchedLVL1;
-                res.status(200).json(afterSearch);
               } else {
-                const afterSearch = moreSearchedLVL1;
-                res.status(200).json(afterSearch);
+                const search8 = firstSearch.filter((item) =>
+                  item.name.toLowerCase().includes(thirdWord)
+                );
+                if (search8.length > 0) {
+                  res.status(200).json(search8);
+                } else {
+                  res.status(200).json(firstSearch);
+                }
               }
             } else {
-              const afterSearch = searched;
-              res.status(200).json(afterSearch);
+              const search4 = items.filter((item) =>
+                item.name.toLowerCase().includes(secondWord)
+              );
+              if (search4.length > 0) {
+                const search5 = search4.filter((item) =>
+                  item.name.toLowerCase().includes(thirdWord)
+                );
+                if (search5.length > 0) {
+                  res.status(200).json(search5);
+                } else {
+                  res.status(200).json(search4);
+                }
+              } else {
+                const search7 = items.filter((item) =>
+                  item.name.toLowerCase().includes(thirdWord)
+                );
+                if (search7.length > 0) {
+                  res.status(200).json(search7);
+                } else {
+                  res.status(200).json([]);
+                }
+              }
             }
-          } else if (moreSearched.length === 0) {
-            const afterSearch = searched;
-            res.status(200).json(afterSearch);
           } else {
-            const afterSearch = moreSearched;
-            res.status(200).json(afterSearch);
+            const firstWord = text.split(" ")[0];
+            const secondWord = text.split(" ")[1];
+
+            const search10 = items.filter((item) =>
+              item.name.toLowerCase().includes(firstWord)
+            );
+            if (search10.length > 0) {
+              const search11 = search10.filter((item) =>
+                item.name.toLowerCase().includes(secondWord)
+              );
+              if (search11.length > 0) {
+                res.status(200).json(search11);
+              } else {
+                res.status(200).json(search10);
+              }
+            } else {
+              const search12 = items.filter((item) =>
+                item.name.toLowerCase().includes(secondWord)
+              );
+              if (search12.length > 0) {
+                res.status(200).json(search12);
+              } else {
+                res.status(200).json([]);
+              }
+            }
           }
         } else {
-          const searched = items.filter((item) =>
+          const search = items.filter((item) =>
             item.name.toLowerCase().includes(text)
           );
-          const afterSearch = searched;
-          res.status(200).json(afterSearch);
+          res.status(200).json(search);
         }
       } else {
         res.status(200).json(items);
